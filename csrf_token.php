644d38f7ade0cdef7e9e0efea0d538ddaffaf625
Paws <131801287+paws1234@users.noreply.github.com>
2024-05-10 23:15:47 +0800

    Update csrf_token.php

--- csrf_token.php
+++ csrf_token.php
@@ -1,17 +1,13 @@
 <?php
-
-
-
+//Mga Headers ni ash/lloyd
 header("Content-Security-Policy: default-src 'self'");
 header("X-Content-Type-Options: nosniff");
 header("X-Frame-Options: DENY");
 header("X-XSS-Protection: 1; mode=block");
 header("Strict-Transport-Security: max-age=31536000; includeSubDomains; preload");
-
-
 session_start();
 
-
+//chuy chuy ni na csrf token shit sayop ni di ni legit pero mahog ni siya as another way para di ma attack 
 $csrf_token = bin2hex(random_bytes(32));
 $_SESSION['csrf_token'] = $csrf_token;
 
