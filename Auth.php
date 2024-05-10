fbcbfa24baec2cd8d89a5978c52e16d48e9144a2
Paws <131801287+paws1234@users.noreply.github.com>
2024-05-10 23:13:24 +0800

    Update Auth.php

--- Auth.php
+++ Auth.php
@@ -1,8 +1,10 @@
 <?php
+//Mga Headers ni ash/lloyd
 header("Content-Security-Policy: default-src 'self'");
 header("X-Content-Type-Options: nosniff");
 header("X-Frame-Options: DENY");
 header("X-XSS-Protection: 1; mode=block");
+//Ari ang class na mo Authenticate sa user credentials
 class Auth {
     private $conn;
 
@@ -33,7 +35,7 @@ class Auth {
             }
         }
   
-     
+     //chuy chuy rani para di ma bruteforce not-proven pa
         usleep(rand(50000, 1000000)); 
         
         return false;
